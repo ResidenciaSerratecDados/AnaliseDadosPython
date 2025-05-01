d3e30eb018ba2840ff7a2fe8687f25cb4939461b
MURILO409 <murilodasdoresalves@gmail.com>
2025-04-30 21:05:58 -0300

    Add JupyterNotebook directory

--- JupyterNotebook/exercicio16.py
+++
@@ -1,7 +0,0 @@
-#Exercício 16: Verifique se um número é par ou ímpar usando if ternário.
-
-n = int(input('Escreva um número: '))
-resto = (n % 2)
-
-resultado = "É par" if resto == 0 else "É ímpar"
-print(resultado)
--- JupyterNotebook/exercicio23.py
+++
@@ -1,7 +0,0 @@
-## Exercício 23: Calcule a soma dos números de 1 a 100."""
-
-vetor = []
-for X in range(1,101):
-    vetor = (vetor + [X])
-    soma = sum(vetor)
-print(soma)
