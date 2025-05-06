77f267b7bcb60fe226368512fa9885b8dfdbc6bf
MURILO409 <murilodasdoresalves@gmail.com>
2025-05-06 20:15:05 -0300

    Finalização da Conta Bancária

--- exercicios_aula/sistema_bancario.py
+++ exercicios_aula/sistema_bancario.py
@@ -18,18 +18,18 @@ class ContaBancaria:
 # - conta2, saldo
 # - saque, depósito e transferência
 
-    def depositar(self, valor):
+    def depositar(self, valor):       
         self.saldo += valor
         print("Depósito de " + str(valor) + " realizado com sucesso. Saldo: " + str(self.saldo))
          
-    def sacar(self, valor):
+    def sacar(self, valor):       
         if self.saldo + self.limite >= valor:
             self.saldo -= valor
             print("Saque de " + str(valor) + " realizado com sucesso. Saldo: " + str(self.saldo))
         else:
             print("Saldo insuficiente. Tente um valor menor.")
-
-    def transferir(self, destino, valor):
+            
+    def transferir(self, destino, valor):        
         if self.saldo >= valor:
             self.sacar(valor)
             destino.depositar(valor)
@@ -37,22 +37,24 @@ class ContaBancaria:
         else:
             print("Saldo insuficiente para transferir. Tente um valor menor.")
 
-conta1 = ContaBancaria(numero_conta='12548-9',titular='Carlos Drumond de Andrade',saldo=19870)
-conta2 = ContaBancaria(numero_conta='54783-9',titular='Fernando Pessoa',saldo=7521)
-
-conta1.ver_saldo() #1
-print('\n')
-conta2.ver_saldo() #2
-print('\n')
-conta1.depositar(1808) #2
-print('\n')
-conta1.sacar(2300) #2
-print('\n')
-conta1.transferir(conta2, 1520) #2
-print('\n')
-conta1.ver_saldo() #2
-print('\n')
-conta2.ver_saldo() #2
+conta1 = ContaBancaria(numero_conta=125489,titular='Carlos Drumond de Andrade',saldo=19870)#1
+conta2 = ContaBancaria(numero_conta=547839,titular='Fernando Pessoa',saldo=7521)#2
+#conta2.ver_saldo() #2
 
 # 3 - Criar um menú que permite ao usuário realizar N transações até que ele opte por sair.
 # - utilizar while como menú
+while True:
+    print('\n1-Consultar Saldo\n2-Depósito\n3-Transferência\n4-Saque\n5-Sair')
+    op= int(input('\nEscolha uma opção: '))
+    
+    if op == 1:
+        conta1.ver_saldo()
+    elif op == 2:
+        conta1.depositar(valor = float(input('Quanto você deseja depositar: ')))
+    elif op == 3:
+        conta1.transferir(conta2,valor = float(input('Quanto você deseja transferir: ')))
+    elif op == 4:
+        conta1.sacar(valor = float(input('Quanto você deseja sacar: ')))
+    elif op == 5:
+        print('Obrigado!!!')
+        break
