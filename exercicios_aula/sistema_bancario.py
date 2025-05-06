4b8115ed4de8a2f1e59b7f90161ad583ea3719ca
MURILO409 <murilodasdoresalves@gmail.com>
2025-05-05 21:23:42 -0300

    Iniciar Sistema Bancário

---
+++ exercicios_aula/sistema_bancario.py
@@ -0,0 +1,5 @@
+# 1 - Criar lógica que verifica saldo e uma conta corrente
+
+# 2 - Criar uma segunda conta corrente para simular saque, deposito e transferência
+
+# 3 - Criar um menú que permite ao usuário realizar N transações até que ele opte por sair.
