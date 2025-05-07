2da8ddc1ef7696874323160b7abbc839b3feb021
MURILO409 <murilodasdoresalves@gmail.com>
2025-05-07 20:03:42 -0300

    Adição de Conta Bancária em Classes

---
+++ exercicios_aula/SistemaBancario/conta.py
@@ -0,0 +1,31 @@
+class ContaCorrente:
+    def __init__(self, numConta, saldo = 0.0):
+        self.numConta = numConta
+        self.saldo = saldo
+        
+    def consultar_saldo(self):
+        print(f'O saldo da conta {self.numConta}: R$ {self.saldo:.2f}')#exibir 2 casas decimais
+        
+    def sacar(self, valor):
+        if 0 < valor <= self.saldo:
+            self.saldo -= valor
+            print(f'Saque realizado com sucesso!\nO saldo atual da conta 1: {self.saldo:.2f}')
+        else:
+            print('Saldo insuficiente ou inválido!')
+            
+    def depositar(self, valor):
+        if valor > 0:
+            self.saldo += valor
+            print(f"Depósito realizado com sucesso.\nO saldo atual da conta 1: {self.saldo:.2f}")
+        else:
+            print('Operação inválida.')
+            
+    def transferir(self, destinatario, valor):
+        if 0 < valor <= self.saldo:
+            self.saldo -= valor
+            destinatario.saldo += valor
+            print(f'Transferência realizada com sucesso!\nO saldo atual da conta: {self.saldo:.2f}')
+        else:
+            print('Saldo insuficiente ou inválido!')
+        
+        
