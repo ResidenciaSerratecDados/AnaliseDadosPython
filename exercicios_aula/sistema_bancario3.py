3b309f191141cb801dd9cf06f641afe0e2f2547f
MURILO409 <murilodasdoresalves@gmail.com>
2025-05-07 18:56:47 -0300

    Saldo Bancário com Funções

--- exercicios_aula/sistema_bancario3.py
+++ exercicios_aula/sistema_bancario3.py
@@ -1,17 +1,27 @@
-conta1 = {'conta1': 125489,'saldo1': 19870}#1
-conta2 = {'conta2': 547839,'saldo2': 7521}#2
+#conta1 = {'conta1': 125489,'saldo1': 19870}#1
+#conta2 = {'conta2': 547839,'saldo2': 7521}#2
+saldo1 = 19870
+saldo2 = 1480
 
 while True:
+    print('\n1-Consultar Saldo\n2-Depósito\n3-Transferência\n4-Saque\n5-Sair')
     op= int(input('\nEscolha uma opção: '))
     
     if op == 1:
-       
+       print(f'O saldo da conta 1: {saldo1}')
     elif op == 2:
-        
+       valor = float(input('Escolha o valor do depósito: '))
+       saldo1 += valor
+       print("Depósito realizado com sucesso.\nO saldo atual da conta 1: " + str(saldo1))
     elif op == 3:
-        
+       valor = float(input('Escolha o valor da transferência: '))
+       saldo1 -= valor
+       saldo2 += valor
+       print(f'Transferência realizada com sucesso!\nO saldo atual da conta 1: {saldo1}')
     elif op == 4:
-        
+       valor = float(input('Escolha o valor do saque: '))
+       saldo1 -= valor
+       print(f'Saque realizado com sucesso!\nO saldo atual da conta 1: {saldo1}')
     elif op == 5:
         print('Obrigado!!!')
         break
@@ -22,15 +32,16 @@ def exibir_menu():
 def sacar(valor):
     global saldo
     saldo -= valor
-    print("Saque de " + str(valor) + " realizado com sucesso. Saldo: " + str(saldo))
+    print(f'Saque realizado com sucesso!\nO saldo atual da conta 1: {saldo}')
     
 def depositar(valor):
     global saldo
     saldo += valor
-    print("Depósito de " + str(valor) + " realizado com sucesso. Saldo: " + str(saldo))
+    print("Depósito realizado com sucesso.\nO saldo atual da conta 1: " + str(saldo))
     
 def transferir(valor):
     global saldo, destinatario
     saldo -= valor
     destinatario += valor
     print(f'Transferência realizada com sucesso!\nO saldo atual da conta: {saldo}')
+    
